82589883e8820d41d1a8da683047c3e18c6102b8
RamziJabali <ramzieljabali@gmail.com>
2021-01-25 22:12:31 -0700

    Program 1

---
+++ C++/Program1/Program1.cpp
@@ -0,0 +1,42 @@
+/*@Author: Ramzi Eljabali,
+ *@Purpose: Very simple program
+ * that introduces me and answers a dew questions.
+ * @Date:1/25/2021
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Name: Ramzi Eljabali" << endl;
+    cout << "Course Work:" << endl;
+    cout << "CIS 163AA Java Programming: Level I" << endl;
+    cout << "CSC 110AB Intro Computer Science(Java)" << endl;
+    cout << "CSC 120 Digital Design Fundamentals" << endl;
+    cout << "CSC 205AB OOP & Data Structures" << endl;
+    cout << "CSC 230 Comp Org & Assembly Language" << endl;
+    cout << "Professional Background: None in this industry" << endl;
+    cout << "Other Computer Experience: I did a few computer" << endl;
+    cout << "New Horizon courses" << endl;
+    cout << "Programming languages used:" << endl;
+    cout << "Java" << endl;
+    cout << "HTML, JAVA, CSS" << endl;
+    cout << "Scratch" << endl;
+    cout << "Personal goals: " << endl;
+    cout << "1. Get comfortable with another language." << endl;
+    cout << "2. Try and get an A in this class." << endl;
+    cout << "3. Be on top of this class." << endl;
+    cout << "Concerns about this course:" << endl;
+    cout << "1. Getting stuck on a certain topic for too long" << endl;
+    cout << "2. Getting overwhelmed with work." << endl;
+    cout << "Current classes:" << endl;
+    cout << "CSC 240" << endl;
+    cout << "CSC 230" << endl;
+    cout << "MAT 227" << endl;
+    cout << "ABS 253" << endl;
+    cout << "Favorite Program I made:" << endl;
+    cout << "A multiplayer ship sinking game, that was designed" << endl;
+    cout << "using MVVM" << endl;
+    cout << "Personal Interest: Physical exercise" << endl;
+    return 0;
+}
