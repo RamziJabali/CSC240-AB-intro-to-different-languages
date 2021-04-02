b5f1ac833c0f6948b64e594082d5aea7c174c933
RamziJabali <ramzieljabali@gmail.com>
2021-04-02 00:35:59 -0700

    Program 10

---
+++ Prolog/Programming Assignments/knowledgeBaseRulers.pl
@@ -0,0 +1,56 @@
+lifespan(abdulaziz_ibn_saud, 1875, 1953).
+lifespan(fahad_saud_bin_abdulaziz_al_saud, 1902, 1969).
+lifespan(faisal_bin_abdulaziz_al_saud, 1906, 1975).
+lifespan(khaled_bin_abdulaziz_al_saud, 1913, 1982).
+lifespan(fahd_bin_abdulaziz_al_saud, 1923, 2005).
+lifespan(abdullah_bin_abdulaziz_al_saud, 2005, 2015).
+lifespan(salman_bin_abdulaziz_al_saud, 1935, alive).
+lifespan(faisal_bin_salman, 1970, alive).
+lifespan(mohammed_bin_salman, 1940, alive).
+lifespan(sultan_bin_salman_al_saud, 1956, alive).
+lifespan(saud_bin_faisal_al_saud, 1940, 2015).
+lifespan(khaled_bin_faisal_al_saud, 1940, alive).
+lifespan(mohammed_bin_fahd_al_saud, 1950, alive).
+lifespan(miteb_bin_abdullah_al_saud, 1952, alive).
+lifespan(sultan_bin_abdulaziz_al_saud, 1928, 2011).
+lifespan(khaled_bin_sultan_al_saud, 1949, alive).
+lifespan(bandar_bin_sultan_al_Saud, 1949, alive).
+lifespan(mohammed_bin_fahd_al_saud, 1950, alive).
+lifespan(talal_bin_abdulaziz_al_saud, 1931, 2018).
+lifespan(alwaleed_bin_talal_al_saud, 1955, alive).
+lifespan(nayef_bin_abdulaziz_al_saud, 1934, 2012).
+lifespan(saud_bin_nayef_al_saud, 1956, alive).
+lifespan(mohammed_bin_nayef_al_saud, 1959, alive).
+lifespan(muqrin_bin_abdulaziz_al_saud, 1945, 2012).
+
+rulerOf(abdulaziz_ibn_saud, saudi_arabia, 1932, 1953).
+rulerOf(fahad_saud_bin_abdulaziz_al_saud, saudi_arabia, 1953, 1964).
+rulerOf(faisal_bin_abdulaziz_al_saud, saudi_arabia, 1964, 1975).
+rulerOf(khaled_bin_abdulaziz_al_saud, saudi_arabia, 1975, 1982).
+rulerOf(fahd_bin_abdulaziz_al_saud, saudi_arabia, 1982, 2005).
+rulerOf(abdullah_bin_abdulaziz_al_saud, saudi_arabia, 1982, 2005).
+rulerOf(salman_bin_abdulaziz_al_saud, saudi_arabia, 2015, current).
+
+parentOf(abdulaziz_ibn_saud,fahad_saud_bin_abdulaziz_al_saud).
+parentOf(abdulaziz_ibn_saud, faisal_bin_abdulaziz_al_saud).
+parentOf(abdulaziz_ibn_saud, khaled_bin_abdulaziz_al_saud).
+parentOf(abdulaziz_ibn_saud, fahd_bin_abdulaziz_al_saud).
+parentOf(abdulaziz_ibn_saud, abdullah_bin_abdulaziz_al_saud).
+parentOf(abdulaziz_ibn_saud, sultan_bin_abdulaziz_al_saud).
+parentOf(abdulaziz_ibn_saud, talal_bin_abdulaziz_al_saud).
+parentOf(abdulaziz_ibn_saud, nayef_bin_abdulaziz_al_saud).
+parentOf(abdulaziz_ibn_saud, salman_bin_abdulaziz_al_saud).
+parentOf(abdulaziz_ibn_saud, muqrin_bin_abdulaziz_al_saud).
+parentOf(fahad_saud_bin_abdulaziz_al_saud, amal_bint_fahad).
+parentOf(faisal_bin_abdulaziz_al_saud, saud_bin_faisal_al_saud).
+parentOf(faisal_bin_abdulaziz_al_saud, khaled_bin_faisal_al_saud).
+parentOf(abdullah_bin_abdulaziz_al_saud, miteb_bin_abdullah_al_saud).
+parentOf(fahd_bin_abdulaziz_al_saud, mohammed_bin_fahd_al_saud).
+parentOf(salman_bin_abdulaziz_al_saud, faisal_bin_salman).
+parentOf(salman_bin_abdulaziz_al_saud, mohammed_bin_salman).
+parentOf(salman_bin_abdulaziz_al_saud, sultan_bin_salman_al_saud).
+parentOf(sultan_bin_abdulaziz_al_saud, khaled_bin_sultan_al_saud).
+parentOf(sultan_bin_abdulaziz_al_saud, bandar_bin_sultan_al_Saud).
+parentOf(talal_bin_abdulaziz_al_saud, alwaleed_bin_talal_al_saud).
+parentOf(nayef_bin_abdulaziz_al_saud, saud_bin_nayef_al_saud).
+parentOf(nayef_bin_abdulaziz_al_saud, mohammed_bin_nayef_al_saud).
