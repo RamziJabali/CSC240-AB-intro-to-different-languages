9ba18f882fcbe922925813f2f5d9760cb066f1da
RamziJabali <ramzieljabali@gmail.com>
2021-04-02 16:58:32 -0700

    removing program 10 from repo

--- Scheme/Programming Assignments/Program10.rkt
+++
@@ -1,7 +0,0 @@
-(define (getBagCount list item)
-  (cond
-    ((null? list) 0)
-    ((string=? (car list) item) (+ 1 (getBagCount (cdr list) item)))
-    (else (+ 0 (getBagCount (cdr list) item)))
-   )
-  )
